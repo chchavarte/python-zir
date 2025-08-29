5bf5c7abed82209314157cb23802ceb5c2ce1ef8
chchavarte <cvart13@gmail.com>
2025-08-30 00:32:40 +0530

    commit

--- zir.py
+++ zir.py
@@ -1,5 +1,12 @@
 class Calculator:
     def __init__(self):
+        self.walls = []
 
+    def get_wall(self):
+        pass
 
-
+    def convert_to_mm(self):
+        pass
+    
+    def wall_area(self):
+        pass
